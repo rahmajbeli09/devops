a6382f533927d207a5587bb283220bce9b4277cb
rahmajbeli09 <118625710+rahmajbeli09@users.noreply.github.com>
2025-10-02 17:02:35 +0100

    Update DepartmentController.java

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -16,8 +16,7 @@ import java.util.List;
 public class DepartmentController {
     private IDepartmentService departmentService;
 
-    @GetMapping("/getAllDepartment")
-    public List<Department> getAllDepartment() { return departmentService.getAllDepartments(); }
+   
 
     @GetMapping("/getDepartment/{id}")
     public Department getDepartment(@PathVariable Long id) { return departmentService.getDepartmentById(id); }
